6d65d88669a4075fa486f8f4647b1f964d74ad45
Felipe Santos <lipe.fssantos063@gmail.com>
2021-10-21 15:37:29 -0300

    trocando por variavel

--- exp_planck/exp_planck.ino
+++ exp_planck/exp_planck.ino
@@ -18,7 +18,7 @@ ll int start_time = millis();
 void setup() {Serial.begin(9600);}
 
 void loop() {   
-  R_LDR = (DDP*R_dvt/(analogRead(pinR_dvt)*(5/1023))) - R_dvt;
+  R_LDR = (DDP*R_dvt/(analogRead(pinR_dvt)*(DDP/1023))) - R_dvt;
   
   cte_planck = pow(DDP, 2) * (1/R_LDR) * (1/freq) * (1000 * (millis()-start_time));
   
